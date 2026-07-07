675a72211e476648a8afbf709da40af8bb40f157
vaish02112000 <107236918+vaish02112000@users.noreply.github.com>
2026-07-07 19:50:59 -0400

    Update solutions.py

--- day02/solutions.py
+++ day02/solutions.py
@@ -16,3 +16,41 @@ def count_safe_reports(filename):
 
 if __name__ == "__main__":
     print(count_safe_reports("input.txt"))
+
+
+def is_safe(levels):
+    increasing = all(
+        1 <= levels[i + 1] - levels[i] <= 3
+        for i in range(len(levels) - 1)
+    )
+    decreasing = all(
+        1 <= levels[i] - levels[i + 1] <= 3
+        for i in range(len(levels) - 1)
+    )
+
+    return increasing or decreasing
+
+def is_safe_with_dampener(levels):
+    if is_safe(levels):
+        return True
+
+    for i in range(len(levels)):
+        new_levels = levels[:i] + levels[i + 1:]
+        if is_safe(new_levels):
+            return True
+
+    return False
+
+def count_safe_reports(filename):
+    safe_count = 0
+
+    with open(filename) as file:
+        for line in file:
+            levels = list(map(int, line.split()))
+            if is_safe_with_dampener(levels):
+                safe_count += 1
+
+    return safe_count
+
+if __name__ == "__main__":
+    print(count_safe_reports("input.txt"))
